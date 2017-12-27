2fc985f34eadf1eabb629151b50cc1ff8ca4566e
Anurag Tomer <anuragtomer@github.com>
2017-12-27 19:55:42 +0530

    Adding non_decreasing_array

---
+++ leetcode/non_decreasing_array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution{
+    public:
+        bool checkPossibility(vector<int>& nums) {
+            
+        }
+};
+
+int main() {
+    Solution sol;
+    vector <int> nums = {4, 2, 3};
+    cout << sol.checkPossibility(nums) << endl;
+    return 0;
+}
+
