715236800f15672e7d98ca7d27815eaa9b5136af
Anurag Tomer <anuragtomer@github.com>
2018-01-10 20:01:24 +0530

    adding remove duplicates.cpp. Removes duplicates in sorted array.

--- leetcode/remove_duplicates.cpp
+++ leetcode/remove_duplicates.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution{
     public:
-
+        int removeDuplicates(vector<int> &num) {
+            int i = 1;
+            for(;i < num.size(); i++) {
+                if(num[i] == num[i - 1]) {
+                    num.erase(num.begin() + i);
+                    i--;
+                }
+            }
+            for (i = 0; i < num.size(); i++) {
+                cout << num[i] << " ";
+            }
+            cout << endl;
+            return num.size();
+        }
 };
 int main() {
     Solution sol;
+    vector<int> a = {1,1,2,4};
+    int si = sol.removeDuplicates(a);
+    cout << si << endl;
 
+            for (int i = 0; i < a.size(); i++) {
+                cout << a[i] << " ";
+            }
     return 0;
 }
 
