eeb52b105de47835c2e6a24a7628e12281d7c0eb
Anurag Tomer <anuragtomer@github.com>
2019-04-02 19:13:12 +0530

    Checking Atom

--- leetcode/prefixesDivBy5.cpp
+++ leetcode/prefixesDivBy5.cpp
@@ -10,10 +10,12 @@ class Solution{
         res.clear();
         long int current = 0;
         for(int i = 0; i < A.size(); i++) {
-            // We need to save only the remainder in current.
-            // We would remember that from the last time this remainder was not divisible by 5. 
-            // Now, multiply with 2, and next input and see if this is divisible by 5.
-            // Saving only remainder would save space as well and save possibility of overflowing.
+            /* We need to save only the remainder in current.
+             * We would remember that from the last time this remainder was
+             * not divisible by 5. Now, multiply with 2, and next input and see
+             * if this is divisible by 5. Saving only remainder would save space
+             * as well and save possibility of overflowing.
+             */
             current = ((current << 1) + A[i]) % 5;
             res.push_back(current % 5 == 0);
         }
@@ -25,12 +27,10 @@ int main() {
     vector <int> a = {1,1,0,0,0,1,0,0,1};
     vector<bool> res;
     res = sol.prefixesDivBy5(a);
-    for (int i = 0; i < res.size(); i++) 
+    for (int i = 0; i < res.size(); i++)
         if (res[i])
             cout << "true ";
         else
             cout << "false ";
     return 0;
 }
-
-
