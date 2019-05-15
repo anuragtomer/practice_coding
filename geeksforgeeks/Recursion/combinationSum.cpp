e4b83ff4fbc951f4790f8e989371f712b18f68d3
Anurag Tomer <anuragtomer@github.com>
2019-05-15 06:50:25 +0530

    GfG - Combination Sum 2 - Sum of subset with duplicate elements in input

---
+++ geeksforgeeks/Recursion/combinationSum.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    void combinationSumRecursive(vector <int> arr, int idx, int remainingSum, vector <int> currVec, set <vector <int> > &result) {
+        if (remainingSum == 0) {
+            result.insert(currVec);
+            return;
+        }
+        if (idx >= arr.size() || remainingSum < 0)
+            return;
+         /* Including (idx)th element. */
+        currVec.push_back(arr[idx]);
+        combinationSumRecursive(arr, idx + 1, remainingSum - arr[idx], currVec, result);
+         /* Excluding (idx)th element. */
+        currVec.pop_back();
+        combinationSumRecursive(arr, idx + 1, remainingSum, currVec, result);
+    }
+    set <vector <int> > combinationSum(vector <int> arr, int sum) {
+        set <vector <int> > result;
+        result.clear();
+        vector <int> currVec;
+        currVec.clear();
+        sort(arr.begin(), arr.end());
+         /* Including 0th element. */
+        currVec.push_back(arr[0]);
+        combinationSumRecursive(arr,  1, sum - arr[0], currVec, result);
+         /* Excluding 0th element. */
+        currVec.pop_back();
+        combinationSumRecursive(arr,  1, sum, currVec, result);
+        return result;
+    }
+};
+
+int main(){
+    Solution sol;
+    int T;
+    cin >> T;
+    while(T) {
+        int num;
+        cin >> num;
+        vector <int> arr;
+        arr.clear();
+        while(num) {
+            int val;
+            cin >> val;
+            arr.push_back(val);
+            num--;
+        }
+        int sum;
+        cin >> sum;
+        set <vector <int> > result;
+        result = sol.combinationSum(arr, sum);
+        if (result.size() != 0) {
+            for (vector <int> ans: result) {
+                cout << "(";
+                for (int i = 0; i < ans.size(); i++) {
+                    cout << ans[i];
+                    if (i != ans.size() - 1)
+                        cout << " ";
+                }
+                cout << ")";
+            }
+            cout << endl;
+        } else {
+            cout << "Empty" << endl;
+        }
+        T--;
+    }
+    return 0;
+}
