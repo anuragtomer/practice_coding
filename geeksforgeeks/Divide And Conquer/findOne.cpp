cc4a57331402476d48b473c884b614ae5fd00473
Anurag Tomer <anuragtomer@github.com>
2019-05-17 21:46:50 +0530

    GfG - findOne - Find one element not repeated in sorted array.

---
+++ geeksforgeeks/Divide And Conquer/findOne.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findOne(vector <int> arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (i + 1 < arr.size() && arr[i] == arr[i + 1]) {
+                i++;
+            } else {
+                return arr[i];
+            }
+        }
+        return 0;
+    }
+};
+
+int main(){
+    Solution sol;
+    int T;
+    cin >> T;
+    while(T) {
+        int num;
+        cin >> num;
+        vector <int> arr;
+        arr.clear();
+        while(num) {
+            int val;
+            cin >> val;
+            arr.push_back(val);
+            num--;
+        }
+        cout << sol.findOne(arr) << endl;
+        T--;
+    }
+    return 0;
+}
