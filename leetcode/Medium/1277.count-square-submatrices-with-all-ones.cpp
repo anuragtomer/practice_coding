a9f2e16c935bf7ef8bae05044d63b0204cbc63f9
Anurag Tomer <anuragtomer@github.com>
2021-04-27 08:45:40 +0530

    Pushing practice code 27 Apr

--- leetcode/Medium/1277.count-square-submatrices-with-all-ones.cpp
+++ leetcode/Medium/1277.count-square-submatrices-with-all-ones.cpp
@@ -1,1 +1,37 @@
-21.count-square-submatrices-with-all-ones.cpp
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+   public:
+    int countSquares(vector<vector<int>> &matrix) {
+        int H = matrix.size(), W = matrix[0].size();
+        vector<vector<int>> dp = matrix;
+        for (int r = 1; r < H; ++r) {
+            for (int c = 1; c < W; ++c) {
+                if (dp[r][c] != 0 && dp[r - 1][c] != 0 && dp[r][c - 1] != 0 && dp[r - 1][c - 1] != 0) {
+                    dp[r][c] = 1 + min(dp[r - 1][c], min(dp[r][c - 1], dp[r - 1][c - 1]));
+                }
+            }
+        }
+        // Sum individual points
+        int count = 0;
+        for (auto row : dp) {
+            for (auto val : row) {
+                count += val;
+            }
+        }
+        return count;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<vector<int>> matrix = {{0, 1, 1, 1}, {1, 1, 1, 1}, {0, 1, 1, 1}};
+    assert(sol.countSquares(matrix) == 15);
+    return 0;
+}
+
--- leetcode/Medium/338.counting-bits.cpp
+++ leetcode/Medium/338.counting-bits.cpp
@@ -1,1 +1,42 @@
-28.count-bits.cpp
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    vector<int> countBits(int num) {
+        if (num == 0)
+            return {0};
+        if (num == 1)
+            return {0, 1};
+        vector<int> result;
+        result.resize(num + 1);
+        result[0] = 0, result[1] = 1;
+        for (int i = 2; i <= num; i = 2 * i) {
+            for (int j = 0; j < i / 2 && ((i + j) <= num); ++j)
+                result[i + j] = result[i / 2 + j];
+            for (int j = i / 2; j < i && ((i + j) <= num); ++j)
+                result[i + j] = 1 + result[i / 2 + j];
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> output = sol.countBits(2);
+    vector<int> result = {0, 1, 1};
+    assert(output.size() == result.size());
+    for (int i = 0; i < output.size(); ++i)
+        assert(output[i] == result[i]);
+    output = sol.countBits(5);
+    result = {0, 1, 1, 2, 1, 2};
+    assert(output.size() == result.size());
+    for (int i = 0; i < output.size(); ++i)
+        assert(output[i] == result[i]);
+
+    return 0;
+}
+
