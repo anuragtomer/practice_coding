8ed45fa9b6586df22059e9d221358f60d5f8a27e
Anurag <anuragtomer@users.noreply.github.com>
2022-03-21 20:03:20 +0530

    Updated 560. Subarray Sum Equals K

--- leetcode/Medium/560.subarray-sum-equals-k.cpp
+++ leetcode/Medium/560.subarray-sum-equals-k.cpp
@@ -1,1 +1,15 @@
-../30-day-challenge/April/22. subarray-sum.cpp
+class Solution {
+public:
+  int subarraySum(vector<int>& nums, int k) {
+    int count = 0, currentSum = 0;
+    unordered_map<int, int> hash;
+    hash[0] = 1;
+    for (int right = 0; right < nums.size(); ++right) {
+      currentSum += nums[right];
+      if (hash.find(currentSum - k) != hash.end())
+        count += hash[currentSum - k];
+      hash[currentSum]++;
+    }
+    return count;
+  }
+};
